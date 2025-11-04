4fe79a4de2fd0243c94892fbca1805db0ba0461d
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-11-04 20:35:52 -0300

    new method in HomeController to test intelij debbug tool and new dev tool dependency

--- store/src/main/java/com/matheusdev/store/controller/HomeController.java
+++ store/src/main/java/com/matheusdev/store/controller/HomeController.java
@@ -1,3 +1,5 @@
+package com.matheusdev.store.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -5,6 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String index() {
+        String viewName = getViewName();
+        return viewName;
+        // return "index.html";
+    }
+
+    private String getViewName() {
         return "index.html";
+//        return "index2";
     }
 }
